4feb75f7386dae7d916b2202332c7020cc71e527
ishrat144 <81430061+ishrat144@users.noreply.github.com>
2021-11-09 13:01:57 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Card from "./Card";
+function App() {
+  return (
+    <div>
+    <h1 className="heading">My Contact</h1>
+    <Card />
+    </div>
+    );
+  }  
+export default App;
---
+++ Card.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Card() {
+    return(
+            <div>
+              <div className="card">
+                <div className="top">
+                <h2 className="name">Sajal</h2>
+        <img src= "https://www.google.com/imgres?imgurl=https%3A%2F%2Fwallpaperaccess.com%2Ffull%2F5252325.jpg&imgrefurl=https%3A%2F%2Fwallpaperaccess.com%2Fsajal-aly&tbnid=J8kELBRIGyQtiM&vet=12ahUKEwjn3f_M8cHzAhUlsksFHX-AALIQMygjegUIARDnAQ..i&docid=RTGCxysJ8W0ckM&w=859&h=1009&q=Sajal%20aly%20pic&hl=en&ved=2ahUKEwjn3f_M8cHzAhUlsksFHX-AALIQMygjegUIARDnAQ" alt="avatar_img"/>
+        </div>
+                <div className="bottom">
+                  <p className="info">+123 456 789</p>
+                  <p className="info">my@gmail.com</p>
+                </div>
+              </div>
+            </div>
+          );
+        }
+
+        export default Card;
